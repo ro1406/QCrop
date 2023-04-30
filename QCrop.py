20982ca16c69726dbcc5a0d8feb0eb9172a26503
Rohan <rohanmitra8@gmail.com>
2023-04-30 10:33:39 +0400

    Update EM code

--- QCrop.py
+++ QCrop.py
@@ -53,11 +53,21 @@ class QCrop:
     def generateGraph(self,molecule,lower_bound,upper_bound,step):
         '''
         Generates the graph for the given molecule by testing the interatomic distance going from lower_bound to upper_bound by step.
-        Parameters:
-        molecule -> str: Name of the molecule (eg: H2)
-        lower_bound -> float: Starting interatomic distance to begin
-        upper_bound -> float: Ending interatomic distance
-        step -> float: Step for varying interactomic distance
+
+        Parameters
+        ----------
+        molecule : str
+            Name of the molecule (eg: H2).
+        lower_bound : float
+            Starting interatomic distance to begin.
+        upper_bound : float
+            Ending interatomic distance.
+        step : float
+            Step for varying interactomic distance.
+
+        Returns
+        -------
+        None.
         '''
         # quantum setup - choose simulator and optimiser
         self.atom=molecule
@@ -97,5 +107,83 @@ class QCrop:
         plt.ylabel('Energy')
         plt.legend()
         plt.show()
+        
+    def getHam(self,molecule, dist):
+        #from qiskit_nature.units import DistanceUnit
+        #from qiskit_nature.second_q.drivers import PySCFDriver
+        #from qiskit_nature.second_q.mappers.qubit_converter import QubitConverter
+        #from qiskit_nature.second_q.mappers import ParityMapper
+        #from qiskit.opflow import TwoQubitReduction
+        BeH2 = load_Molecule(molecule, dist)
+        qubitham = BeH2[0]
+        allcoef=[float(''.join(x.split('*')[0].split())) for x in str(qubitham).split('\n')]
+        allops=[''.join(x.split('*')[1].split()) for x in str(qubitham).split('\n')]
+        return allcoef,allops    
+        
+    
+    def readOutVQE(self.coeffs,op_strings):
+        import numpy as np
+        import scipy.optimize as opt
+        from qiskit import transpile,QuantumCircuit
+        from qiskit.circuit.library import TwoLocal
+        from qiskit.providers.fake_provider import FakeAthens
+        import mthree
+        def opstr_to_meas_circ(op_str):
+            num_qubits = len(op_str[0])
+            circs = []
+            for op in op_str:
+                qc = QuantumCircuit(num_qubits)
+                for idx, item in enumerate(op):
+                    if item == 'X':
+                        qc.h(num_qubits-idx-1)
+                    elif item == 'Y':
+                        qc.sdg(num_qubits-idx-1)
+                        qc.h(num_qubits-idx-1)
+                circs.append(qc)
+            return circs
+        ansatz = TwoLocal(2, rotation_blocks='ry', entanglement_blocks='cx')
+        meas_circs = opstr_to_meas_circ(op_strings)
+        full_circs = [ansatz.compose(meas_circs[kk]).measure_all(inplace=False) for kk in range(len(meas_circs))]
+        meas_strings = [string.replace('X', 'Z').replace('Y', 'Z') for string in op_strings]
+        backend = FakeAthens()
+        trans_circs = transpile(full_circs, backend)  
+        num_params = ansatz.num_parameters
+        #params = np.random.uniform(0,5,num_params) #np.array([1.22253725, 0.39053752, 0.21462153, 5.48308027,2.06984514, 3.65227416, 4.01911194, 0.35749589])
+        params = np.array([1.22253725, 0.39053752, 0.21462153, 5.48308027,
+                       2.06984514, 3.65227416, 4.01911194, 0.35749589])
+        params += 0.05*(np.random.random(params.shape[0])-0.5)
+        mit = mthree.M3Mitigation(backend)
+        mit.cals_from_system([0,1])
+        def vqe_func(params, *args):
+            bound_circs = [circ.bind_parameters(params) for circ in trans_circs]
+            counts = backend.run(bound_circs, shots=4096).result().get_counts()
+            quasis = mit.apply_correction(counts, [0,1])
+            energy = np.sum(coeffs*quasis.expval(meas_strings))
+            return energy
+    
+        res = opt.minimize(vqe_func, params, method = 'COBYLA')
+        return res.fun
+        
+    def ErrorMitigatedCircuit(self, molecule,distance):
+        '''
+        Using the error mitigated circuit using Measurement Error Mitigation.
+        
+        Parameters
+        ----------
+        molecule : str
+            Name of molecule (eg: H2).
+        distance : float
+            Intermolecular distance.
+
+        Returns
+        -------
+        float
+            Total energy of the configuration.
+
+        '''
+        coefs,ops=self.getHam(molecule,distance)
+        return self.readOutVQE(coefs,ops)
+        
+        
                 
         
